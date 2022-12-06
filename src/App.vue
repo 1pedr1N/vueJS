fe7f2483e93b4d5293d7d0aaed1569d8498b0c05
Pedro Henrique <pedrinfnn@gmail.com>
2022-12-06 16:01:17 -0300

    :fire:

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <NavBar />
   <router-view />
+  <FooterApp />
 </template>
 <script>
 import NavBar from "./components/Navbar.vue";
+import FooterApp from "./components/FooterApp.vue";
 export default {
   name: "App",
   components: {
     NavBar,
+    FooterApp,
   },
 };
 </script>
---
+++ src/components/FooterApp.vue
@@ -0,0 +1,13 @@
+<!-- eslint-disable prettier/prettier -->
+<!-- eslint-disable prettier/prettier -->
+<template>
+    <footer>
+        <p>Footer</p>
+    </footer>
+</template>
+<!-- eslint-disable prettier/prettier -->
+<script>
+export default {
+    name: "FooterApp",
+};
+</script>
